f83bfb9abd2c17e1449e811c6f34a5f35a9b00b9
Jeadine29 <143826069+Jeadine29@users.noreply.github.com>
2024-03-08 06:54:16 +0800

    update js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,21 +1,15 @@
 import flatpickr from 'flatpickr';
-import { Report } from 'notiflix/build/notiflix-report-aio';
-
 import 'flatpickr/dist/flatpickr.min.css';
-
-//------------------------ Refs -----------------------------------
+import Notiflix from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
-  buttonStart: document.querySelector('[data-start]'),
-  days: document.querySelector('[data-days]'),
-  hours: document.querySelector('[data-hours]'),
-  minutes: document.querySelector('[data-minutes]'),
-  seconds: document.querySelector('[data-seconds]'),
+  inputText: document.querySelector('#datetime-picker'),
+  btn: document.querySelector('button'),
+  spanDay: document.querySelector('span[data-days]'),
+  spanHour: document.querySelector('span[data-hours]'),
+  spanMinute: document.querySelector('span[data-minutes]'),
+  spanSecond: document.querySelector('span[data-seconds]'),
 };
-
-refs.buttonStart.disabled = true;
-
-//---------------- flatpickr Options -------------------------------
 
 const options = {
   enableTime: true,
@@ -23,58 +17,71 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates[0] < new Date()) {
-      Report.failure('Please choose a date in the future');
-
-      refs.buttonStart.disabled = true;
+    if (selectedDates[0] - new Date() < 0) {
+      return Notiflix.Notify.failure('Please choose a date in the future');
     } else {
-      refs.buttonStart.disabled = false;
-      refs.buttonStart.addEventListener('click', () => {
-        onTimer(selectedDates[0]);
-      });
+      refs.btn.disabled = false;
     }
   },
 };
 
-flatpickr('#datetime-picker', options);
+const date = flatpickr(refs.inputText, options);
 
-//--------------------------------------------------------------
-
-function render(param) {
-  const { days, hours, minutes, seconds } = param;
-  refs.days.textContent = days < 10 ? `0${days}` : days;
-  refs.hours.textContent = hours < 10 ? `0${hours}` : hours;
-  refs.minutes.textContent = minutes < 10 ? `0${minutes}` : minutes;
-  refs.seconds.textContent = seconds < 10 ? `0${seconds}` : seconds;
-}
-
-function onTimer(ms) {
-  refs.buttonStart.disabled = true;
-  const timerId = setInterval(() => {
-    const timerValue = ms - Date.now();
-
-    render(convertMs(timerValue));
-    if (timerValue <= 1000) {
-      clearInterval(timerId);
+const timer = {
+  intervalId: null,
+  isActive: false,
+  start() {
+    if (this.isActive) {
+      return;
     }
-  }, 1000);
-}
+    this.isActive = true;
+    this.intervalId = setInterval(() => {
+      const userInputDate = date.selectedDates[0];
+      const currentTime = new Date();
+      const timeOff = userInputDate - currentTime;
+      const time = convertMs(timeOff);
+      updateInterfaceTimer(time);
+      if (
+        time.days === 0 &&
+        time.hours === 0 &&
+        time.minutes === 0 &&
+        time.seconds === 0
+      ) {
+        this.stop();
+      }
+    }, 1000);
+  },
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+    refs.btn.disabled = true;
+  },
+};
+
+refs.btn.addEventListener('click', () => {
+  timer.start();
+});
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
   return { days, hours, minutes, seconds };
+}
+
+function addInFrontZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function updateInterfaceTimer({ days, hours, minutes, seconds }) {
+  refs.spanDay.textContent = `${addInFrontZero(days)}`;
+  refs.spanHour.textContent = `${addInFrontZero(hours)}`;
+  refs.spanMinute.textContent = `${addInFrontZero(minutes)}`;
+  refs.spanSecond.textContent = `${addInFrontZero(seconds)}`;
 }
